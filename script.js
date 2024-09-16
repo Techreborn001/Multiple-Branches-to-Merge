6ee768a518b7615e99e2fc030e0e7f387eda96b7
immanuel Mbugua <you@example.com>
2024-09-16 17:29:52 +0300

    Pushing script.js to the remote repo

---
+++ script.js
@@ -0,0 +1,7 @@
+// $("h1").css("color", "red");
+// $("body").css("backgroundColor", "red");
+
+$(".icon").click(function () {
+    $("body").toggleClass("darkMode");
+    $(".icon").toggleClass("bxs-moon ");
+});
